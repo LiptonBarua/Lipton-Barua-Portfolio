403cd7ad142d01d4110ffc3f97b01fb7085f2d08
unknown <liptonbarua274@gmail.com>
2023-06-27 11:52:48 +0600

    project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: 'class',
   theme: {
